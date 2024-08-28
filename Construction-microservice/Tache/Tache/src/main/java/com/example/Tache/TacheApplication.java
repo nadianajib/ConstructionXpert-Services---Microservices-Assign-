60fa027f61376d911a3f05b10cf2fb6a84e063e6
nadianajib <nadianajib832@gmail.com>
2024-08-28 12:19:07 +0100

    créer projet Tache

---
+++ Construction-microservice/Tache/Tache/src/main/java/com/example/Tache/TacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacheApplication.class, args);
+	}
+
+}
