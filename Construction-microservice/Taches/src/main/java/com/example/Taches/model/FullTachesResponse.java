46807c0902d704b53d7cd1e3ebdc4e308ca554d2
nadianajib <nadianajib832@gmail.com>
2024-08-29 11:38:02 +0100

    update

---
+++ Construction-microservice/Taches/src/main/java/com/example/Taches/model/FullTachesResponse.java
@@ -0,0 +1,20 @@
+package com.example.Taches.model;
+
+import com.example.Taches.model.enums.Statut;
+import lombok.*;
+
+import java.sql.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FullTachesResponse {
+    private String description;
+    private Date dateDebut;
+    private Date dateFin;
+    private Statut statut;
+//    private List<Ressources> ressources;
+}
