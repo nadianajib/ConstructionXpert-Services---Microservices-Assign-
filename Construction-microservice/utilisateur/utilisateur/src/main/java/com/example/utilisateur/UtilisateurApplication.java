f4114ea931165ab31cddbf2510ae877b42964d4d
nadianajib <nadianajib832@gmail.com>
2024-09-06 16:48:33 +0100

    update securiter avec gateway

--- Construction-microservice/utilisateur/utilisateur/src/main/java/com/example/utilisateur/UtilisateurApplication.java
+++ Construction-microservice/utilisateur/utilisateur/src/main/java/com/example/utilisateur/UtilisateurApplication.java
@@ -2,8 +2,10 @@ package com.example.utilisateur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UtilisateurApplication {
 
 	public static void main(String[] args) {
